a562decd4a7579d08fd9e1ce2c87f2e15b8a3a1c
airidabu <airida.buinickaite@gmail.com>
2026-04-30 20:31:29 +0300

    added moods array, naming change

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
 import "./App.css";
+
+const MOODS = [
+  {
+    value: "tired",
+    emoji: "😵"
+  },
+  {
+    value: "unhappy",
+    emoji: "😞"
+  },
+  {
+    value: "chill",
+    emoji: "😎"
+  },
+  {
+    value: "happy",
+    emoji: "😊"
+  }
+]
+
 function App() {
   return (
-    <div className="body">
+    <div className="container">
       <header>
         <h1>Mood Tracker</h1>
       </header>
       <main>
         <h2>Choose Your Mood Today</h2>
         <div>
-          <button>&#128565;</button>
-          <button>&#128542;</button>
-          <button>&#128524;</button>
-          <button>&#128513;</button>
+          <button>{MOODS[0].emoji}</button>
+          <button>{MOODS[1].emoji}</button>
+          <button>{MOODS[2].emoji}</button>
+          <button>{MOODS[3].emoji}</button>
         </div>
       </main>
       <footer>
