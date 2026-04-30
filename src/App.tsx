d42368d05f57ff44d7800145c5da04168549dedb
airidabu <airida.buinickaite@gmail.com>
2026-04-30 14:17:12 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import "./App.css";
+function App() {
+  return (
+    <div className="body">
+      <header>
+        <h1>Mood Tracker</h1>
+      </header>
+      <main>
+        <h2>Choose Your Mood Today</h2>
+        <div>
+          <button>Mood 1</button>
+          <button>Mood 2</button>
+          <button>Mood 3</button>
+          <button>Mood 4</button>
+        </div>
+      </main>
+      <footer>
+        Created by Airida
+      </footer>
+    </div>
+  )
+}
+
+export default App;
